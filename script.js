968b81b1bfc29f9ccef3a3cced71ce400d8e05a8
Anas Abubakar <anasabubakar2015@gmail.com>
2022-06-15 10:35:50 +0100

    Fixed linters

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const closeMenu = document.querySelector('.close-menu');
 const hambuggerIcon = document.querySelector('.menu-icon');
 const links = document.querySelectorAll('.menu-link');
 
-hambuggerIcon.addEventListener ('click', function (openDisplay){
+hambuggerIcon.addEventListener('click', (openDisplay) => {
   openDisplay.preventDefault();
   hambuggerMenu.style.display = 'flex';
 });
 
-closeMenu.addEventListener ('click', function (){
+closeMenu.addEventListener('click', () => {
   hambuggerMenu.style.display = 'none';
 });
 
-links.forEach(function (link){
-  link.addEventListener ('click', function (){
+links.forEach((link) => {
+  link.addEventListener('click', () => {
     hambuggerMenu.style.display = 'none';
   });
 });
