003d7d67521b171673b404e8a326742197bda21e
Anas Abubakar <anasabubakar2015@gmail.com>
2022-06-15 08:20:11 +0100

    Fixed linter errors

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const hambuggerMenu = document.querySelector('.hambugger');
 const closeMenu = document.querySelector('.close-menu');
-const header = document.querySelector('.header');
 const hambuggerIcon = document.querySelector('.menu-icon');
 const links = document.querySelectorAll('.menu-link');
 
-hambuggerIcon.addEventListener('click', function(event){
-  event.preventDefault();
+hambuggerIcon.addEventListener('click', function (openDisplay){
+  openDisplay.preventDefault();
   hambuggerMenu.style.display = 'flex';
-
 });
-closeMenu.addEventListener('click', function(){
+
+closeMenu.addEventListener('click', function (){
   hambuggerMenu.style.display = 'none';
 });
-links.forEach(function(link){
-  link.addEventListener('click', function(){
+
+links.forEach(function (link){
+  link.addEventListener('click', function (){
     hambuggerMenu.style.display = 'none';
   });
 });
